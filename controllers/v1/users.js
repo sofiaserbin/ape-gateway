1a4798c4fa2a8b16f26c4f5a7b9d731d476e0e19
Sofia Serbina <serbina@student.chalmers.se>
2023-11-13 14:58:40 +0300

    Setup API endpoints documentation #2

---
+++ controllers/v1/users.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+/**
+ * Get /v1/users/{id}
+ * @summary Returns user by id
+ * @tags users
+ * @return {object} 200 - Success response
+ * @return {object} 404 - user id not found
+ */
+router.get("/:id",
+    //TODO: not implemented yet
+);
+
+
+
+
--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 import express from "express"
 import * as mqtt from "mqtt"
+import expressJSDocSwagger from "express-jsdoc-swagger";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const apiDocsRoute = "/api-docs";
 
 const client = mqtt.connect(process.env.BROKER_URL)
 
 const app = express()
 const port = process.env.PORT || 3000
 
+const options ={
+    info: {
+        version: "1.0.0",
+        title: "Entoothiast",
+    },
+    baseDir: __dirname,
+    filesPattern: "./**/*.js",
+    // URL where SwaggerUI will be rendered. Default. /api-docs
+    swaggerUIPath: apiDocsRoute,
+    exposeSwaggerUI: true,
+
+    exposeApiDocs: false,
+    // Open API JSON Docs endpoint.
+    apiDocsPath: "/api-docs",
+    notRequiredAsNullable: false,
+    swaggerUiOptions: {},
+    multiple: true,
+};
+
+expressJSDocSwagger(app)(options);
+
+
 app.get('/', (req, res) => {
     res.send('Hi from api-gateway')
 })
 
+
+
+
+
 client.on("connect", () => {
     console.log("api-gateway connected to broker")
     console.log(`Broker URL: ${process.env.BROKER_URL}`)
+    
 
     app.listen(port, () => {
         console.log(`API-Gateway running on http://localhost:${port}`)
+        console.log(`API Docs running on http://localhost:${port}${apiDocsRoute}`)
     })
 });
