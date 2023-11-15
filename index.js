3fb36ba486b07693260ea90ccb07b2e68daf6591
Michael Koenig <koenigmichael2002@gmail.com>
2023-11-15 13:16:53 +0100

    Add mqtt req-res example #3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as mqtt from "mqtt"
 import expressJSDocSwagger from "express-jsdoc-swagger";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import MqttRequest from "mqtt-request"
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,11 +11,12 @@ const __dirname = path.dirname(__filename);
 const apiDocsRoute = "/api-docs";
 
 const client = mqtt.connect(process.env.BROKER_URL)
+const mqttReq = new MqttRequest.default(client);
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const options ={
+const options = {
     info: {
         version: "1.0.0",
         title: "Entoothiast",
@@ -40,14 +42,21 @@ app.get('/', (req, res) => {
     res.send('Hi from api-gateway')
 })
 
-
-
+app.get("/demo", (req, res) => {
+    mqttReq.request("demo", // topic "demo"
+        // callback that is called when the other serice replies
+        (payload) => {
+            return res.send(payload)
+        },
+        // payload that is sent to the other service
+        JSON.stringify({ message: "Hi from API-Gateway..." }))
+})
 
 
 client.on("connect", () => {
     console.log("api-gateway connected to broker")
     console.log(`Broker URL: ${process.env.BROKER_URL}`)
-    
+
 
     app.listen(port, () => {
         console.log(`API-Gateway running on http://localhost:${port}`)
