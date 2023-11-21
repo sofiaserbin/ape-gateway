914546431a33a2855d6c945e273c67425670d2b5
Milena Maćkowiak <milena.mackowiak7@gmail.com>
2023-11-21 12:26:53 +0100

    Majority of user routes implemented #4

--- controllers/v1/users.js
+++ controllers/v1/users.js
@@ -7,9 +7,20 @@ const router = require("express").Router();
  * @return {object} 200 - Success response
  * @return {object} 404 - user id not found
  */
-router.get("/v1/users/:userId",
-    //TODO: not implemented yet
-);
+router.get("/v1/users/:userId", getUserById = async (req,res) => {
+    
+    try {
+        const userID = req.params.id;
+        const userToFind = await Users.findById(userID);
+        if (!userToFind) {
+            res.status(404).josn({ message: "User ID not found."});
+        }
+        res.status(200).json(userToFind);
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error."})
+    }
+});
+
  /**
  * Get /v1/users/{userId}/notifications
  * @summary Returns all notifications of a user by id
@@ -17,9 +28,22 @@ router.get("/v1/users/:userId",
  * @return {object} 200 - Success response
  * @return {object} 404 - user id not found
  */
-router.get("/v1/users/:userId/notifications",
-//TODO: not implemented yet
-);
+router.get("/v1/users/:userId/notifications", getUserNotifications = async (req, res) => {
+
+try {
+    const userID = req.params.id;
+    const userToFind = await Users.findById(userID);
+    if (!userToFind) {
+        res.status(404).json({ message: "User ID not found."})
+    }
+
+    const notifications = await Notifications.exec();
+    res.status(200).json(notifications);
+
+} catch (err) {
+    res.status(500).json({ message: "Internal server error."})
+}
+});
 
 
 /**
@@ -29,8 +53,23 @@ router.get("/v1/users/:userId/notifications",
  * @return {object} 200 - Success response
  * @return {object} 404 - user id not found
  */
-router.get("/v1/users/:userId/appointments",
-    //TODO: not implemented yet
+router.get("/v1/users/:userId/appointments", getUserAppointments = async (req, res) => {
+
+try {
+    const userID = req.params.id;
+    const userToFind = await Users.findById(userID);
+    if (!userToFind) {
+        res.status(404).json({ message: "User ID not found."})
+    }
+
+    const appointments = await Appointments.exec();
+    res.status(200).json(appointments);
+
+} catch (err) {
+    res.status(500).json({ message: "Internal server error."})
+}
+}
+    
 );
 
 
@@ -53,9 +92,25 @@ router.post("v1/users/login",
  * @return {object} 201 - Success response
  * @return {object} 400 - Bad request response
  */
-router.post("/v1/users/register",
-    //TODO: not implemented yet
-);
+router.post("/v1/users/register", registerUser = async (req, res) => {
+  
+    console.log(req);
+    const users = new Users({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password,
+        clinic_id: req.body.clinic,
+        user_role: req.body.role,
+    });
+
+    try {
+        const userToSave = await users.save();
+        res.status(201).json(userToSave);
+    } catch (err) {
+        res.status(400).json({ message: "User not registered, please try again"});
+    }
+
+});
 
 
 
