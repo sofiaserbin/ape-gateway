e78ce84bda91677133d99aabc3bf0e806c92df22
Michael Koenig <koenigmichael2002@gmail.com>
2023-12-27 17:55:21 +0100

    Add route to get logs with pagination #14

---
+++ controllers/v1/logs.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router()
+import { mqttReq } from "../../index.js"
+
+/**
+ * Get /v1/logs
+ * @summary Returns all logs
+ * @tags logs
+ * @param {number} query.offset - Pagination offset 
+ * @param {number} query.limit - Pagination limit
+ * @return {object} 200 - Success response
+ * @return {object} 400 - Invalid offset or limit
+ */
+router.get("/", (req, res, next) => {
+    const queryParams = {
+        offset: req.query.offset,
+        limit: req.query.limit
+    };
+
+    mqttReq.request(
+        "v1/logging/read",
+        (payload) => {
+            req.mqttResponse = payload;
+            return next();
+        },
+        JSON.stringify(queryParams)
+    );
+});
+
+export default router
