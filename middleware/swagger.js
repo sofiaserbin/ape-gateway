85692e4799bfb5382a096bb37ba367a059601785
Michael Koenig <koenigmichael2002@gmail.com>
2024-01-03 15:01:07 +0200

    Put middleware into separate files

---
+++ middleware/swagger.js
@@ -0,0 +1,23 @@
+import path from "path"
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const apiDocsRoute = "/api-docs";
+export const options = {
+    info: {
+        version: "1.0.0",
+        title: "Entoothiast",
+    },
+    baseDir: __dirname,
+    filesPattern: "./**/*.js",
+    // URL where SwaggerUI will be rendered. Default. /api-docs
+    swaggerUIPath: apiDocsRoute,
+    exposeSwaggerUI: true,
+
+    exposeApiDocs: false,
+    // Open API JSON Docs endpoint.
+    apiDocsPath: "/api-docs",
+    notRequiredAsNullable: false,
+    swaggerUiOptions: {},
+    multiple: true,
+};
