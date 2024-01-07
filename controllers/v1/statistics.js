86e5a0a5859bfbbfd5a799a2011aedfa0ceefca5
Michael Koenig <koenigmichael2002@gmail.com>
2024-01-07 17:32:04 +0200

    Add admin statistics routes #22

--- controllers/v1/statistics.js
+++ controllers/v1/statistics.js
@@ -4,12 +4,11 @@ import { mqttReq } from "../../index.js"
 
 /**
  * Get /v1/statistics/most-used-dentist/users/{userId}
- * @summary Returns all clinics
+ * @summary Returns the most used dentist this year by the user
  * @tags clinics
  * @return {object} 200 - Success response
  */
 router.get("/most-used-dentist/users/:userId", async (req, _, next) => {
-    console.log("hi")
     mqttReq.request("v1/statistics/most-used-dentist/users/read",
         (payload) => {
             req.mqttResponse = payload
@@ -22,18 +21,51 @@ router.get("/most-used-dentist/users/:userId", async (req, _, next) => {
 
 /**
  * Get /v1/statistics/appointments-in-year/users/:userId
- * @summary Returns all clinics
+ * @summary Number of appointments the user has in the current year
  * @tags clinics
  * @return {object} 200 - Success response
  */
 router.get("/appointments-in-year/users/:userId", async (req, _, next) => {
-    console.log("hi")
     mqttReq.request("v1/statistics/appointments-in-year/users/read",
         (payload) => {
             req.mqttResponse = payload
             return next()
         },
         JSON.stringify({ token: req.token, userId: req.params.userId })
+    )
+});
+
+
+/**
+ * Get /v1/statistics/users-active
+ * @summary Number of timeslot searches in the last 5 minutes
+ * @tags clinics
+ * @return {object} 200 - Success response
+ */
+router.get("/number-searches", async (req, _, next) => {
+    mqttReq.request("v1/statistics/number-searches/read",
+        (payload) => {
+            req.mqttResponse = payload
+            return next()
+        },
+        JSON.stringify({ token: req.token })
+    )
+});
+
+
+/**
+ * Get /v1/statistics/timeslots-available
+ * @summary Number of timeslots available
+ * @tags clinics
+ * @return {object} 200 - Success response
+ */
+router.get("/timeslots-available", async (req, _, next) => {
+    mqttReq.request("v1/statistics/timeslots-available/read",
+        (payload) => {
+            req.mqttResponse = payload
+            return next()
+        },
+        JSON.stringify({ token: req.token })
     )
 });
 
